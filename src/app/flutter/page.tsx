eccbf3855d857ee586c0b1a832cf9b01262ecec5
Matthew-uk <esaduviedede@gmail.com>
2025-02-07 12:10:35 +0100

    Added flutterwave payment -v1.0

--- src/app/flutter/page.tsx
+++ src/app/flutter/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 import FlutterwaveButton from '@/components/custom/FlutterwaveButton';
+import { useUserStore } from '@/store/store';
 
 export default function PaymentPage() {
+  const { id } = useUserStore();
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Complete Payment</h1>
       <FlutterwaveButton
+        userId={id}
         amount={1000}
         email="user@example.com"
         name="John Doe"
